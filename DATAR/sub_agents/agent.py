a548084d0a388105b68ca2a57af1a0b008ae7c32
MangleRojo <administrador@manglerojo.org>
2025-11-04 09:29:16 -0500

    Feat: apertura de directorios de trabajo para la integración de DATAR

---
+++ DATAR/sub_agents/agent.py
@@ -0,0 +1,14 @@
+from google.adk.agents.llm_agent import Agent
+from google.adk.models.lite_llm import LiteLlm
+import os
+
+root_agent = Agent(
+    model=LiteLlm(
+        model="openrouter/minimax/minimax-m2:free",  # Especifica el modelo con prefijo 'openrouter/'
+        api_key=os.getenv("OPENROUTER_API_KEY"),  # Lee la API key del entorno
+        api_base="https://openrouter.ai/api/v1"   # URL base de OpenRouter
+    ),
+    name='Gente_Montaña',
+    description='Un agente que siempre saluda desde la Montaña.',
+    instruction='Siempre saluda desde la Montaña.',
+)
