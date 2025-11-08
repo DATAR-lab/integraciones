e61432caf7c010969e5d62deae2bbbb70145d112
Diego Rojas <guatilam@gmail.com>
2025-11-08 13:20:42 -0500

    feat: reordena flujo de agentes, incluyendo un bucle y nuevas instrucciones

--- DATAR/datar/sub_agents/Gente_Normal/agent.py
+++ DATAR/datar/sub_agents/Gente_Normal/agent.py
@@ -1,41 +1,55 @@
 from google.adk.agents.llm_agent import Agent
-from google.adk.agents import ParallelAgent, SequentialAgent
+from google.adk.agents import ParallelAgent, SequentialAgent, LoopAgent
 from google.genai import types
 
 from .utils import leer_instrucciones
 
-# Definición de los agentes individuales
-agente_normal = Agent(
-    model='gemini-2.5-flash',
-    name='GenteNormal',
-    description=(
-        'Un asistente presto a ayudar e informar '
-        'con datos ambientales de Bogotá'
-    ),
-    instruction=leer_instrucciones(),
-    output_key='normal_response',
-)
+# ==========
+# Agentes paralelos
+# ==========
 
 # Agente especializado en interpretar respuestas usando solo emojis
 agente_interprete_emojis = Agent(
     model='gemini-2.5-flash',
     name='GenteInterpreteDeEmojis',
     description=(
-        'Recibe la solicitud del usuario y retorna una '
+        'Recibe una interacción y retorna una '
         'interpretación con sólo emojis'
     ),
     instruction=leer_instrucciones("ins_emoji_agent.md"),
-    output_key='emoji_response',
+    output_key='respuesta_emojis',
+    generate_content_config=types.GenerateContentConfig(
+        temperature=1.8
+    )
+)
+
+# Agente especializado en responder con narrativas, 
+# dando su perspectiva en texto invitando a interpretar 
+# y generando preguntas.
+agente_interprete_textual = Agent(
+    model='gemini-2.5-flash',
+    name='GenteInterpreteDeTexto',
+    description=(
+        'Recibe una interacción y retorna una '
+        'interpretación invitando a interpretar su respuesta e '
+        'generando preguntas a su interlocutor.'
+    ),
+    instruction=leer_instrucciones("ins_agente_textual.md"),
+    output_key='respuesta_textual',
     generate_content_config=types.GenerateContentConfig(
         temperature=1.6
     )
 )
 
+# ==========
+# Agentes en bucle
+# ==========
+
 # Agente que ejecuta los agentes en paralelo
 agente_paralelizador = ParallelAgent(
     name='GenteParalelizador',
     description='Corre múltiples agentes en paralelo.',
-    sub_agents=[agente_normal, agente_interprete_emojis],
+    sub_agents=[agente_interprete_emojis, agente_interprete_textual],
 )
 
 # Agente que combina las respuestas de los agentes paralelos
@@ -47,15 +61,43 @@ agente_fusionador = Agent(
         'y las combina en una sola respuesta coherente.'
     ),
     instruction=leer_instrucciones("ins_merger_agent.md"),
+    output_key='respuesta_fusionadora'
 )
 
-# Agente secuencial que primero ejecuta los agentes 
-# en paralelo y luego combina sus respuestas
-agente_encauzador_secuencial = SequentialAgent(
-    name="GenteEncauzador",
+# Agente que crea un bucle de interpretación paralela 
+# y reinterpretación fusionadora.
+agente_bucle = LoopAgent(
+    name="GenteBucle",
     sub_agents=[agente_paralelizador, agente_fusionador],
-    description="Coordina agentes en paralelo y luego combina sus respuestas."
+    max_iterations=2,
+    description="Crea un bucle de interpretación y reinterpretación",
 )
+
+# =======
+# Agente de interacción
+# =======
+
+# Definir un agente normal que interactúe con lxs usuarixs
+# y les defina una forma de interactuar
+agente_re_interpretativo = Agent(
+    model='gemini-2.5-flash',
+    name='GenteReInterpretativo',
+    description=(
+        'Un asistente presto a ayudar e informar '
+        'con datos ambientales de Bogotá'
+    ),
+    instruction=leer_instrucciones("ins_re_interpretativo.md"),
+    output_key='normal_response',
+    sub_agents=[agente_bucle]
+)
+
+# # Agente secuencial que primero ejecuta los agentes 
+# # en paralelo y luego combina sus respuestas
+# agente_encauzador_secuencial = SequentialAgent(
+#     name="GenteEncauzador",
+#     sub_agents=[agente_paralelizador, agente_fusionador],
+#     description="Coordina agentes en paralelo y luego combina sus respuestas."
+# )
 
 # Agente raíz que se utilizará para interactuar
-root_agent = agente_encauzador_secuencial
+root_agent = agente_re_interpretativo
